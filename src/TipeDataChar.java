a47054f87c1d4ac8bf4c9d4e0524fa807a649f22
felixyunianto <felixyunianto@gmail.com>
2021-11-15 14:07:13 +0700

    2. Boolean, Char, and String

---
+++ src/TipeDataChar.java
@@ -0,0 +1,15 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char f = 'F';
+        char e = 'E';
+        char l = 'L';
+        char i = 'I';
+        char x = 'X';
+
+        System.out.println(f);
+        System.out.println(e);
+        System.out.println(l);
+        System.out.println(i);
+        System.out.println(x);
+    }
+}
---
+++ src/TipeDataString.java
@@ -0,0 +1,13 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Felix Yunianto";
+        String lastName = "Gunawan";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        System.out.println(fullName);
+    }
+}
